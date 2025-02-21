0e3ee9577b0f7c39350edc376316c46461c37122
Christian Slade <sladec@purdue.edu>
2025-02-20 19:03:47 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -103,7 +103,6 @@ vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
